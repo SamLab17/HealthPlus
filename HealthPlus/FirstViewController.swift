80d7e0e3c34893e07556cf3daeddd972d8e0f7c8
SamLab17 <samlaberge@live.com>
2019-03-06 17:41:44 -0600

    Initial Commit

---
+++ HealthPlus/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HealthPlus
+//
+//  Created by Samuel Laberge on 3/6/19.
+//  Copyright © 2019 Convergent. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HealthPlus/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HealthPlus
+//
+//  Created by Samuel Laberge on 3/6/19.
+//  Copyright © 2019 Convergent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
